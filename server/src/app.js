2147f4d0d1b11f22df2e5b161b26b3d794762434
Huu Ha <yamateeeeeei@gmail.com>
2025-11-18 22:28:52 +0700

    rebuild

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(rateLimit);
 app.use(express.json({ limit: '5mb' }));
 
-app.get('/health', (_, res) => res.status(200).json({ status: "ok!" }))
+app.get('/api/health', (_, res) => res.status(200).json({ status: "ok!" }))
 app.use('/api/v1/files', fileRoutes);
 app.get("/api/events", eventRoutes)
 
