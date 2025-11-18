28de9ff6efe5e293e57b2ed877025922f9b941e7
Huu Ha <yamateeeeeei@gmail.com>
2025-11-18 23:43:58 +0700

    rebuild

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 
 const express = require('express');
-const cors = require('cors');
 
 const fileRoutes = require('./api/routes/fileRoutes');
 const eventRoutes = require('./api/controllers/eventController');
